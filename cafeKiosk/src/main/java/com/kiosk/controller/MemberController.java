a479c84ca5e97699eac9e58ef2cc4cfe474842cb
Rachel-jieun Jeong <84854563+stoveon@users.noreply.github.com>
2021-12-07 20:40:49 +0900

    211207
    로그인, 회원가입 완료

--- cafeKiosk/src/main/java/com/kiosk/controller/MemberController.java
+++ cafeKiosk/src/main/java/com/kiosk/controller/MemberController.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -30,25 +29,43 @@ public class MemberController {
 	}
 	
 	@RequestMapping(value="regist", method=RequestMethod.POST)
-	String registPost(MemberVo member, Model model) throws Exception {
-		MemberVo exist = memberService.checkPhoneNumber(member.getPhone());
-		if(!exist.equals(null)) {
-			
+	String registPost(MemberVo member, String month, String day, RedirectAttributes rttr, HttpSession session) throws Exception {
+		if(member.getName() == null || member.getName() == "" || member.getBirthYear() == "" || month == "" || day == "") {
+			rttr.addFlashAttribute("member", member);			
+			rttr.addFlashAttribute("month", month);
+			rttr.addFlashAttribute("day", day);		
+			rttr.addFlashAttribute("check", "입력하지 않은 정보가 존재합니다.");
+			rttr.addFlashAttribute("next", "next");
+			return "redirect:/cafeCarp/regist";
+		}else {
+			if(month.length() <2) month = "0"+month;
+			if(day.length() <2) day = "0"+day;
+			member.setBirth(month + day);
+			memberService.registMember(member);
+			MemberVo exist = memberService.checkPhoneNumber(member.getPhone());
+			session.setAttribute("member", exist);
+			rttr.addFlashAttribute("success", "hello");			
+			return "redirect:/cafeCarp/order";
 		}
-		memberService.checkPhoneNumber(member.getPhone());
-		memberService.registMember(member);
-		return "user/registForm";
 	}
 	
-	@RequestMapping(value="phoneCheck")
-	String registCheck(MemberVo member, Model model) throws Exception {
-		MemberVo exist = memberService.checkPhoneNumber(member.getPhone());
-		if(!exist.equals(null)) {
-			
+	@RequestMapping(value="phoneCheck", method=RequestMethod.POST)
+	String registCheck(MemberVo member, String month, String day, RedirectAttributes rttr) throws Exception {
+		if(member.getPhone() == null || member.getPhone().length()!=11) {
+			rttr.addFlashAttribute("check", "번호를 알맞게 입력해주세요.");
+			return "redirect:/cafeCarp/regist";			
 		}
-		memberService.checkPhoneNumber(member.getPhone());
-		memberService.registMember(member);
-		return "user/registForm";
+		MemberVo exist = memberService.checkPhoneNumber(member.getPhone());
+		if(exist != null) {
+			rttr.addFlashAttribute("check", "이미 등록된 번호입니다.");
+			return "redirect:/cafeCarp/regist";
+		}
+			rttr.addFlashAttribute("checkOK", "noexist");
+			rttr.addFlashAttribute("member", member);
+			rttr.addFlashAttribute("month", month);
+			rttr.addFlashAttribute("day", day);
+			rttr.addFlashAttribute("next", "next");
+			return "redirect:/cafeCarp/regist";
 	}
 	
 	
@@ -59,7 +76,7 @@ public class MemberController {
 	
 	@RequestMapping(value="login", method=RequestMethod.POST)
 	String login(String phone, HttpSession session, RedirectAttributes rttr) throws Exception {
-		if(phone.equals(null) || phone == "" || phone.isEmpty() || phone.length() < 11) {
+		if(phone.equals(null) || phone == "" || phone.isEmpty() || phone.length()!=11) {
 			return "redirect:/cafeCarp/login";
 		}
 		MemberVo exist = memberService.checkPhoneNumber(phone);
--- cafeKiosk/src/main/webapp/WEB-INF/views/user/registForm.jsp
+++ cafeKiosk/src/main/webapp/WEB-INF/views/user/registForm.jsp
@@ -7,52 +7,70 @@
 <head>
 <meta charset="UTF-8">
 <title>cafeCarp 회원가입</title>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script type="text/javascript">
+var chk = "<c:out value="${checkOK}"/>";
+if(chk === 'noexist'){
+	$(function(){
+		$("#phone").attr("readonly", true);		
+	})
+	alert('사용 가능한 번호입니다.');
+}
+var nx = "<c:out value="${next}"/>";
+if(nx === 'next'){
+	$(function(){
+		$("#subBtn").attr("disabled", false);
+	})
+}
+</script>
 </head>
 <body>
 <jsp:useBean id="now" class="java.util.Date"/>
 <fmt:formatDate value="${now}" pattern="yyyy" var="thisYear"/>
-<form action="<c:url value="/cafeCarp/regist" />" method="post">
+<form method="post">
 <table>
 	<thead></thead>
 	<tbody>
 		<tr>
-			<td colspan="3"><input type="text" name="phone" placeholder="'-'를 제외한 숫자 11자리 입력 "/></td>
-			<td><button onclick="#">중복체크</button></td>
+			<td colspan="3"><input id="phone" name="phone" type="text" value="${member.phone}" placeholder="'-'를 제외한 숫자 11자리 입력 "/></td>
+			<td><button id="phoneCHK" type="submit" onclick="form.action='<c:url value="/cafeCarp/phoneCheck" />'" >중복체크</button></td>
 		</tr>
 		<tr>
-			<td colspan="4"><input type="text" name="name" placeholder="이름"/>
+			<td colspan="4"><input id="name" name="name" type="text" value="${member.name}" placeholder="이름"/>
 		</tr>
 		<tr>
 			<td>생 년 월 일</td>
 			<td>
-				<select name="birthYear" id="year" title="년도">
+				<select id="year" name="birthYear">
 					<option value="">년도</option>
 					<c:forEach var="i" begin="1930" end="${thisYear}" step="1">
-						<option value="${i}">${i}</option>
+						<option value="${i}" <c:if test="${!empty member.birthYear && i == member.birthYear}">selected="selected"</c:if>>${i}</option>
 					</c:forEach>
 				</select>
 			</td>
 			<td>
-				<select name="month" id="month" title="월">
+				<select id="month" name="month">
 					<option value="">월</option>
 					<c:forEach var="i" begin="1" end="12" step="1">
-						<option value="${i}">${i}</option>
+						<option value="${i}" <c:if test="${!empty month && i == month}">selected="selected"</c:if>>${i}</option>
 					</c:forEach>				
 				</select>
 			</td>
 			<td>	
-				<select name="day" id="day" title="일">
+				<select id="day" name="day">
 					<option value="">일</option>				
-					<c:forEach var="i" begin="1" end="31" step="1">
-						<option value="${i}">${i}</option>
+					<c:forEach var="i" begin="1" end="31" step="1">			
+						<option value="${i}" <c:if test="${!empty day && i == day}">selected="selected"</c:if>>${i}</option>
 					</c:forEach>				
 				</select>
 			</td>
-			<c:set var="birth" value="${month += day}"></c:set>
 		</tr>
+		<c:if test="${!empty check}">
+			<tr><td colspan="4"><c:out value="${check}"/></td></tr>
+		</c:if>
 		<tr>
 			<td colspan="2"><button type="button" onclick="history.go(-1);">가입취소</button></td>
-			<td colspan="2"><button type="submit">가입하기</button></td>
+			<td colspan="2"><button id="subBtn" type="submit" onclick="form.action='<c:url value="/cafeCarp/regist" />'" disabled="true">가입하기</button></td>
 		</tr>
 	</tbody>
 </table>
